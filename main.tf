da18f92cc067dfd913723d98d9d620c0af6d9819
Mahima <mahima.sharu@hashicorp.com>
2025-09-19 13:53:57 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Define a random string to create a unique bucket name
+resource "random_string" "bucket_suffix" {
+  length  = 6
+  special = false
+  upper   = false
+}
+
+# Create a private S3 bucket
+resource "aws_s3_bucket" "example" {
+  bucket = "my-unique-bucket-${random_string.bucket_suffix.result}"
+}
+
+# Output the bucket name
+output "bucket_name" {
+  value = aws_s3_bucket.example.bucket
+}
